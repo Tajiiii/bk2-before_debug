ef10f70a238494e328313ea0367427fa79097461
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-02 17:09:50 +0900

    submit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
-    user_path(resource)
+    root_path
   end
 
   def configure_permitted_parameters
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book), notice: "You have created New book successfully."
     else
       @books = Book.all
       render 'index'
@@ -24,13 +24,13 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+
   end
 
 
 
   def update
-    @book = Book.find(params[:id])
+
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -39,15 +39,24 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
+
     @book.destroy
     redirect_to books_path
   end
 
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
+
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
+  end
+
 
 end
