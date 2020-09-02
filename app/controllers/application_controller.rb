596009c248b48e8ef0c10c64a6ceae4b99f0f517
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-02 16:05:30 +0900

    finish-23

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def configure_permitted_parameters
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,20 @@
 class BooksController < ApplicationController
 
+  before_action :authenticate_user!
+
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -33,16 +38,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
